299e289b035d8bb1a2feaba50adb5b19b2581895
yukyo <mrym12000@gmail.com>
2020-08-16 14:24:15 +0900

    add src

---
+++ src/javascripts/main.js
@@ -0,0 +1,25 @@
+import './reactApp.jsx';
+import '../stylesheets/main.scss';
+
+
+// $(function() {
+//   // スクロールのオフセット値
+//   var offsetY = -10;
+//   // スクロールにかかる時間
+//   var time = 500;
+
+//   // ページ内リンクのみを取得
+//   $('button[href^=#]').click(function() {
+//     // 移動先となる要素を取得
+//     var target = $(this.hash);
+//     if (!target.length) return ;
+//     // 移動先となる値
+//     var targetY = target.offset().top+offsetY;
+//     // スクロールアニメーション
+//     $('html,body').animate({scrollTop: targetY}, time, 'swing');
+//     // ハッシュ書き換えとく
+//     window.history.pushState(null, null, this.hash);
+//     // デフォルトの処理はキャンセル
+//     return false;
+//   });
+// });
